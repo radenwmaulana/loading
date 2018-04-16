f229b9fe994ed1939fa0fe1343e1d2349e5ceb53
Raden Maulana <radenwmaulana@gmail.com>
2018-04-17 04:20:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<title>Loading</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/stael.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<body class="body">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<h1 class="mt-5">Laman ITK...</h1>
+		<p class="lead">Silahkan klik button 'Lanjutkan' untuk redirect</p>
+		<div id="myProgress">
+			<div id="myBar"></div>
+		</div>
+		<br>
+		<button class="click" onclick="move()">Click Me</button>
+		</div>
+	</div>
+</div>
+	<script src="js/load.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
